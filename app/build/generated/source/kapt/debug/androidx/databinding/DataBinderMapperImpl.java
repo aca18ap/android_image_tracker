9b9a187fd9f7164734a48ec78c748d3105657e36
egdrei <amtihoan1@sheffield.ac.uk>
2021-12-16 12:43:33 +0000

    removed build files from the index

--- app/build/generated/source/kapt/debug/androidx/databinding/DataBinderMapperImpl.java
+++
@@ -1,7 +0,0 @@
-package androidx.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new uk.ac.shef.oak.com4510.DataBinderMapperImpl());
-  }
-}
--- app/build/generated/source/kapt/debug/uk/ac/shef/oak/com4510/BR.java
+++
@@ -1,5 +0,0 @@
-package uk.ac.shef.oak.com4510;
-
-public class BR {
-  public static final int _all = 0;
-}
